40dd60ee9b20d19e93fd27f2ae6a93c088d11651
Hattan <Hatanazizksa@gmail.com>
2023-02-05 03:09:33 +0300

    uesrname

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-      MyApp()
-  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,15 +9,52 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
+        backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            height: 100,
-            width: 100,
-            margin: EdgeInsets.all(20),
-            padding: EdgeInsets.all(20),
-            color: Colors.white,
-            child: Text('hello'),
+
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+
+              CircleAvatar(
+                radius: 100,
+                backgroundImage: AssetImage('images/hattan.jpg'),
+              ),
+              SizedBox(height: 20),
+              Text(
+                  'hattan Aziz',
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white,
+                ),
+              ),
+
+              Text('----------------------------------------------------------------------------------------------------------------------------------------',
+              style: TextStyle(
+                fontSize: 10,
+                color: Colors.white,
+              )),
+
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'uesrname',
+                  ) ,
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'password',
+                  ) ,
+                ),
+              ),
+            ],
           ),
         ),
       ),
